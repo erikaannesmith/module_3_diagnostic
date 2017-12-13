5dd2439baaa088a8df628e691698aefb601ec6f0
Erika Smith <erikaannesmith@gmail.com>
2017-12-13 09:25:55 -0700

    Write test and begin to TDD- create search controller/index

---
+++ spec/features/user_can_view_stations_given_zip_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "User searches a zip code" do
+  it "they see 10 closest stations" do
+    visit '/'
+    fill_in "q", with: '80203'
+
+    click_button "Locate"
+    expect(current_path).to eq('/search')
+    url = URI.parse(current_url).to_s
+    expect(url).to include('zipcode=80203')
+    expect(page).to have_content('10 Results')
+    within(".stations") do
+      expect(page).to have_selector(".station", count: 10)
+    end
+
+    within(first(".station")) do
+      expect(page).to have_selector(".name")
+      expect(page).to have_selector(".address")
+      expect(page).to have_selector(".fuel_type")
+      expect(page).to have_selector(".distance")
+      expect(page).to have_selector(".access_times")
+    end
+  end
+end
