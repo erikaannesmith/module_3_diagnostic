9653ea8b2bfe91661ea4e6750969eaafdb1fbf79
Erika Smith <erikaannesmith@gmail.com>
2017-12-13 10:04:15 -0700

    begin to push faraday helper to model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,13 @@ class SearchController < ApplicationController
 
   def index
     # byebug
-    @stations = Station.where(zip_code: params[:q])
-    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&fuel_type=ELEC,LPG") do |faraday|
-      faraday.headers["X-API-KEY"] = "BmEoGTxrar0Fc82jBh5uC5ZKTnTdzeEI2MjqyNlL"
-      faraday.adapter Faraday.default_adapter
-    end
 
-
+    @stations = Station.all_by_zipcode(zip_code: params[:q])
+    # @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&fuel_type=ELEC,LPG") do |faraday|
+    #   faraday.headers["X-API-KEY"] = "BmEoGTxrar0Fc82jBh5uC5ZKTnTdzeEI2MjqyNlL"
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    byebug
   end
 
 end
@@ -23,5 +23,17 @@ class Station
     @distance = attributes[:distance]
     @access_times = attributes[:access_times]
   end
+
+  def self.all_by_zipcode(zipcode)
+    faraday_helper
+  end
+
+  def self.faraday_helper
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&radius=6&fuel_type=ELEC,LPG") do |faraday|
+      faraday.headers["X-API-KEY"] = "BmEoGTxrar0Fc82jBh5uC5ZKTnTdzeEI2MjqyNlL"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
 
 end
