e82c776c46fed0a7438661aa7211ea060973d008
Erika Smith <erikaannesmith@gmail.com>
2017-12-13 10:58:52 -0700

    tried to implement VCR.. not really sure if it's working because there are no casettes in my app but test is not breaking...

--- spec/features/user_can_view_stations_given_zip_spec.rb
+++ spec/features/user_can_view_stations_given_zip_spec.rb
@@ -2,24 +2,26 @@ require 'rails_helper'
 
 describe "User searches a zip code" do
   it "they see 10 closest stations" do
-    visit '/'
-    fill_in "zipcode", with: '80203'
+    VCR.use_cassette('station_info', :allow_playback_repeats => true) do
+      visit '/'
+      fill_in "zipcode", with: '80203'
 
-    click_button "Locate"
-    expect(current_path).to eq('/search')
-    url = URI.parse(current_url).to_s
-    expect(url).to include('zipcode=80203')
-    expect(page).to have_content('10 Results')
-    within(".stations") do
-      expect(page).to have_selector(".station", count: 10)
-    end
+      click_button "Locate"
+      expect(current_path).to eq('/search')
+      url = URI.parse(current_url).to_s
+      expect(url).to include('zipcode=80203')
+      expect(page).to have_content('10 Results')
+      within(".stations") do
+        expect(page).to have_selector(".station", count: 10)
+      end
 
-    within(first(".station")) do
-      expect(page).to have_selector(".name")
-      expect(page).to have_selector(".address")
-      expect(page).to have_selector(".fuel_type")
-      expect(page).to have_selector(".distance")
-      expect(page).to have_selector(".access_times")
+      within(first(".station")) do
+        expect(page).to have_selector(".name")
+        expect(page).to have_selector(".address")
+        expect(page).to have_selector(".fuel_type")
+        expect(page).to have_selector(".distance")
+        expect(page).to have_selector(".access_times")
+      end
     end
   end
 end
