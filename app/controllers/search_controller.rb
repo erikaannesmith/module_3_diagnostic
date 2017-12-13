adff17c2901648cbc0b07a83a32e46753163d80b
Erika Smith <erikaannesmith@gmail.com>
2017-12-13 09:35:58 -0700

    Begin to create station model and build out search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,22 @@
 class SearchController < ApplicationController
 
   def index
+    # byebug
+    @stations = Station.where(zip_code: params[:q])
 
   end
-  
+
+end
+
+class Station
+  attr_reader :name, :address, :fuel_type, :distance, :access_times
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @address = attributes[:address]
+    @fuel_type = attributes[:fuel_type]
+    @distance = attributes[:distance]
+    @access_times = attributes[:access_times]
+  end
+
 end
